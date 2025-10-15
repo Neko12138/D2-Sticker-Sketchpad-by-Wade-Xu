cf0a535ad75de913334dd76a42cd6040cc1a082c
Wade Xu <146382470+Neko12138@users.noreply.github.com>
2025-10-15 16:38:31 +0000

    Step1 complete
    add title
    Co-authored-by: Keith Kida <Keith-kida@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+const title = document.createElement("h1");
+title.textContent = "Sticker Sketchpad";
+document.body.appendChild(title);
